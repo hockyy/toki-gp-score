04d20f24b693ed9a90ad1a973bbfed549d484b9b
hocky <hocky.yudhiono@gmail.com>
2021-12-19 10:23:34 +0700

    Add troc-24

--- sc.py
+++ sc.py
@@ -4,7 +4,7 @@ totalScore = dict()
 GPrank = [100, 75, 60, 50, 45, 40, 36, 32, 29, 26, 24, 22, 20, 18, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 finalData = []
 FIRSTGP = 16
-LASTGP = 23
+LASTGP = 24
 
 def main():
     for i in range(FIRSTGP, LASTGP + 1):
